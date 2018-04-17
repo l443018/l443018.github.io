216479dc987817f59aa2556d28b9a4d198b7c2d5
laura_lee <laura_lee@mail.gss.com.tw>
2018-04-18 01:33:24 +0800

    Change header & add font-awesome

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import './assets/font/fontawesome.css';
+import './assets/font/fa-brands.css';
 import Header from './header/header';
 import { ThemeProvider } from 'styled-components';
+import styled from 'styled-components';
+import firebase from 'firebase';
 
 const theme = {
   theme1: { id: 1, bg: '#282855', text: 'white'},
@@ -9,10 +13,79 @@ const theme = {
   theme3: { id: 3, bg: '#d8eec3', text: '#1b5327'},
 };
 
+const AppWrapper = styled.div`
+  &:before {
+    content: '';
+    position: absolute;
+    height: 120vh;
+    width: 100%;
+    background: linear-gradient(to bottom right, black 50%, white 50%);
+    z-index: -1;
+  }
+`;
+
+const Name = styled.div`
+  color: white;
+  display: inline-block;
+  vertical-align: middle;
+  font-size: 100px;
+  height: 100vh;
+  padding: 50px;
+
+  &:before {
+    content: '';
+    height: 100%;
+    width: 0;
+    display: inline-block;
+    vertical-align: middle;
+  }
+`;
+
+const Links = styled.div`
+  text-align: right;
+  position: fixed;
+  bottom: 30px;
+  width: 100%;
+  z-index: 1;
+
+  &:hover {
+    .fab {
+      opacity: 0.5;
+    }
+  }
+
+  .fab {
+    color: black;
+    padding: 10px;
+    cursor: pointer;
+    font-size: 40px;
+    transition: opacity 0.3s;
+
+    &:hover {
+      opacity: 1;
+    }
+  }
+`;
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {theme: 'theme1'};
+
+    var config = {
+      apiKey: "AIzaSyDXLUb7_sZn_u5FW1Sd5KkC5_gh8vCZfYg",
+      authDomain: "l443018-63fc1.firebaseapp.com",
+      databaseURL: "https://l443018-63fc1.firebaseio.com",
+      projectId: "l443018-63fc1",
+      storageBucket: "l443018-63fc1.appspot.com",
+      messagingSenderId: "277479446490"
+    };
+    firebase.initializeApp(config);
+    // firebase.database().ref("test").once("value", a => {
+    //   console.log('a', a.val());
+    // }, b => {
+    //   console.log('b', b);
+    // });
   }
 
   handleSetTheme(theme) {
@@ -21,14 +94,27 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <ThemeProvider theme={theme[this.state.theme]}>
-          <Header onSetTheme={x => this.handleSetTheme(x)}></Header>
-        </ThemeProvider>
-      </div>
+      // 文字 Blog 影音 dance band kyudo 音樂 sing kyudo chado wafuku 程式
+      <ThemeProvider theme={theme[this.state.theme]}>
+        <AppWrapper>
+          <Name>Laura</Name>
+          <Links>
+            <a href="https://linkedin.com/in/l443018/" target="_blank" className="fab fa-linkedin"></a>
+            <a href="https://github.com/l443018" target="_blank" className="fab fa-github"></a>
+            <a href="https://medium.com/@l443018" target="_blank" className="fab fa-medium"></a>
+            <a href="https://www.slideshare.net/l443018" target="_blank" className="fab fa-slideshare"></a>
+            <br />
+            <a href="https://www.facebook.com/l443018" target="_blank" className="fab fa-facebook"></a>
+            <a href="https://www.instagram.com/l443018" target="_blank" className="fab fa-instagram"></a>
+            <a href="http://l443018.blogspot.tw/" target="_blank" className="fab fa-blogger"></a>
+          </Links>
+          {/* <Header onSetTheme={x => this.handleSetTheme(x)}></Header> */}
+          <Header></Header>
+          <Header></Header>
+        </AppWrapper>
+      </ThemeProvider>
     );
   }
 }
 
 export default App;
-
--- src/header/header.js
+++ src/header/header.js
@@ -1,44 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import { hexToRgb } from '../common-method';
 
 const HeaderWrapper = styled.div`
-  padding: 40px;
-  text-align: center;
-  transition: all 0.5s;
-  word-spacing: 10px;
-  letter-spacing: 12px;
-  font-weight: bold;
-  
-  color: ${props => props.theme.text};
-  background-color: ${props => props.theme.bg};
-`;
-
-const buttonActive = `
-  letter-spacing: 20px;
-  opacity: 1;
-  cursor: default;
-`;
-const Button = styled.div`
-  display: inline-block;
-  font-size: 40px;
-  padding: 0 10px;
-  opacity: 0.8;
-  transition: letter-spacing 0.3s;
-  cursor: pointer;
-  
-  &:hover {
-    letter-spacing: 20px;
-  }
-
-  &:first-child {
-    ${props => { return props.theme.id === 1 ? buttonActive : '' }}
-  }
-  &:nth-child(3) {
-    ${props => { return props.theme.id === 2 ? buttonActive : '' }}
-  }
-  &:nth-child(5) {
-    ${props => { return props.theme.id === 3 ? buttonActive : '' }}
-  }
+  height: 100vh;
 `;
 
 export default class Header extends Component {
@@ -49,11 +14,8 @@ export default class Header extends Component {
   render() {
     return (
       <HeaderWrapper {...this.props}>
-        <Button onClick={() => this.setTheme('theme1')}>工程師</Button>
-        <Button>/</Button>
-        <Button onClick={() => this.setTheme('theme2')}>武射系</Button>
-        <Button>/</Button>
-        <Button onClick={() => this.setTheme('theme3')}>裏千家</Button>
+        <div className="test">
+        </div>
       </HeaderWrapper>
     );
   }
